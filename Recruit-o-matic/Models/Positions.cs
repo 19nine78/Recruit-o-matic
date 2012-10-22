7a04eb641d00233c763e6d4ee70d26af643f8db7
19nine78 <19nine78@gmail.com>
2012-10-22 13:41:14 +0100

    More basic setup
    Base ravendb controller added, bootstrap CSS framework installed & a
    local ravendb connectionstring added

---
+++ Recruit-o-matic/Models/Positions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recruit_o_matic.Models
+{
+    public class Positions
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public DateTime ClosingDate { get; set; }
+    }
+}
