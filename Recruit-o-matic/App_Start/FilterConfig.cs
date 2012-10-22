a58a0bfe3872090c1d9a49ad90346518b302a4ad
19nine78 <19nine78@gmail.com>
2012-10-22 12:45:03 +0100

    Initial solution checked in

---
+++ Recruit-o-matic/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Recruit_o_matic
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
