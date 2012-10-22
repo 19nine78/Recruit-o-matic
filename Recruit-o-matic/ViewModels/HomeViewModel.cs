8e8fcb0931494d486542c742dce4ea19b419553e
19nine78 <19nine78@gmail.com>
2012-10-22 23:50:11 +0100

    created home view model

---
+++ Recruit-o-matic/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Recruit_o_matic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recruit_o_matic.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IList<Vacancy> currentPositions { get; set; }
+    }
+}
