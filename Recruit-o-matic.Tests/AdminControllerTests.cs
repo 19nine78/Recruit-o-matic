225f7e4d8798d67cf5a1a4f0181775e9c6a527ed
19nine78 <19nine78@gmail.com>
2012-11-07 13:35:33 +0000

    empty test created

---
+++ Recruit-o-matic.Tests/AdminControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Recruit_o_matic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recruit_o_matic.Controllers;
+using System.Web.Mvc;
+
+namespace Recruit_o_matic.Tests
+{
+    [TestClass]
+    public class AdminControllerTests
+    {
+        [TestMethod]
+        public void HomeMethodUsesConventionToChooseView()
+        {
+            //Arrange
+            var controller = new AdminController();
+
+            //Act
+            var result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(result.ViewName));
+            
+        }
+    }
+}
