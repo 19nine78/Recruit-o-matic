d1786e3e2ffb8f17ce2c5338d78de4d46ce9606c
19nine78 <19nine78@gmail.com>
2012-11-07 14:57:27 +0000

    Markdown Relocated

---
+++ Recruit-o-matic/Scripts/MarkDownDeepInit.js
@@ -0,0 +1,11 @@
+﻿//Setup based on the assumption we're identifying all editors with the .mdd_editor class
+
+$(document).ready(function () {
+    $("textarea.mdd_editor").MarkdownDeep({
+           help_location: "/Scripts/mdd_help.htm",
+           disableTabHandling: true
+       });
+});
+
+
+
